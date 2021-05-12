988305c4dd9e5808d338ba3268e82d4964c0bda0
adepoju2006 <adepojuayomide2006@gmail.com>
2021-05-12 13:40:41 -0700

    first commit

---
+++ loop.js
@@ -0,0 +1,52 @@
+var players = [
+  "Messi",
+  "Ronal",
+  "Peles",
+  "Ibram",
+  "Nacho",
+  "Gress",
+  "Hazar",
+  "Hulks",
+  "Aguar",
+  "Cechs",
+  "Cante",
+];
+var player = [
+  "Ronal",
+  "Cante",
+  "Aguar",
+  "Messi",
+  "Nacho",
+  "Adepo",
+  "Ayomi",
+  "Cechg",
+  "Aguar",
+  "Hulks",
+  "Ibram",
+];
+for (let i = 0; i < players.length; i++) {
+  console.log(
+    players[i] +
+      " " +
+      "  " +
+      " " +
+      " " +
+      " " +
+      " " +
+      " " +
+      " " +
+      " " +
+      " " +
+      " " +
+      " " +
+      " " +
+      " " +
+      " " +
+      " " +
+      " " +
+      " " +
+      " " +
+      " " +
+      player[i]
+  );
+}
